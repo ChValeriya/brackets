77cf558f1d025fa9e554908942b0ea1202e70c7e
Valeriya Chuvak <chva5000@gmail.com>
2023-02-12 23:58:29 +0300

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr;
+  if (!Array.isArray(str)) {
+    arr = str.split('');
+  } else {
+    arr = str;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    bracketsConfig.forEach(element => {
+      if (arr[i] === element[0] && arr [i+1] === element[1]) {
+        arr.splice(i, 2);
+        check(arr, bracketsConfig)
+      }
+    });
+  }
+  if (!arr[0]) {
+    return true
+  } else {
+    return false
+  }
 }
